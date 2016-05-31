b15995a6c24f65c58ac0573cbb9db8cf7f41710e
Alexx Frankos <afrankos@asu.edu>
2016-05-31 14:00:49 -0700

    added javadoc

--- cse360assign2/src/cse360assign2/TestAnalytics.java
+++ cse360assign2/src/cse360assign2/TestAnalytics.java
@@ -1,3 +1,11 @@
+/** @author Alexx Frankos
+ * PIN# 25
+ * Assignment 2 
+ * CSE 360
+ * TestAnalytics is the main class that tests OrderedIntlist and Analytics
+ * it creates an object of type Analytics.
+ * This class calls all the functions in both Analytics and in orderedIntList 
+ */
 package cse360assign2;
 
 import java.util.Scanner;
@@ -8,7 +16,7 @@ public class TestAnalytics
 	   {
 		boolean debugging = false;
 		int debug;
-		
+		Analytics list; 
 		Scanner scan = new Scanner(System.in);
 		
 		System.out.println("Do you want to debug? enter '0' for no or '1' for yes");
@@ -24,7 +32,7 @@ public class TestAnalytics
 			debugging = true;
 		}
 		
-		Analytics list = new Analytics(debugging);
+		list = new Analytics(debugging);
 		
 		try
 		{
@@ -34,32 +42,38 @@ public class TestAnalytics
 				int num = scan.nextInt();
 				list.insertIntoList(num);
 			}
+			
 			while(scan.hasNextInt()== true);
 		}
+		//catches the empty list exception
 		catch(java.util.InputMismatchException e)
 		{
 			
 		}
 		
+		//prints the list
 		list.printAllValuesInList();
-		
 		System.out.println(" ");
 		
+		//calls the mean function
 		System.out.println("the mean: " +list.mean());
 		System.out.println(" ");
 		
+		//calls the median function
 		System.out.println("the median:  " + list.median());
 		System.out.println(" ");
 		
+		//calls the high function
 		System.out.println("the high is:  " + list.high());
 		System.out.println(" ");
 		
+		// calls the low function
 		System.out.println("the low is:  " + list.low());
 		System.out.println(" "); 
 		
+		//calls the numInts function
 		System.out.println("the number of Ints is:  " + list.numInt());
 		System.out.println(" ");
-		
 		
 	   }
 }
